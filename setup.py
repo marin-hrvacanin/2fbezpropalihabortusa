ae0bbedbc6d04709bf0762be21fe8799619b2c30
Marin Hrvaćanin <37810800+marin-hrvacanin@users.noreply.github.com>
2021-01-19 17:00:04 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,8 +11,6 @@ setup(
   download_url = 'https://github.com/marin-hrvacanin/2fbezpropalihabortusa/archive/v_01.tar.gz',    # I explain this later on
   keywords = ['GAY', 'COOL', 'NICE'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
